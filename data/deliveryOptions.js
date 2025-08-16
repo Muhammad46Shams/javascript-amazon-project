8d4d3015c5260f7dc44714b0127e5864a41491a6
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-08-16 02:02:04 +0100

    add functionality to check whether day is weekend then push the delivery day to the week days

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -30,10 +30,36 @@ export function getDeliveryOption(deliveryOptionId)
 
 export function calculateDeliveryDate(deliveryOption)
 {
-    const today = dayjs();
-    const deliveyDate  = today.add(deliveryOption.deliveryDays, 'day');
-    const dateString = deliveyDate.format('dddd, MMMM DD');
-    console.log(dateString, 'date');
+    // const today = dayjs();
     
+    // const deliveyDate  = today.add(deliveryOption.deliveryDays, 'day');
+    // const dateString = deliveyDate.format('dddd, MMMM DD');
+    
+    // return dateString;
+    let remainingDays = deliveryOption.deliveryDays;
+    let deliveryDate = dayjs();
+
+    while (remainingDays > 0) {
+        deliveryDate = deliveryDate.add(1, 'day');
+        console.log(deliveryDate, 'deliveryDate');
+        console.log(remainingDays, 'remainingDays');
+        
+
+        if (!isWeekend(deliveryDate)) {
+        remainingDays--;
+        // This is a shortcut for:
+        // remainingDays = remainingDays - 1;
+        }
+    }
+
+     const dateString = deliveryDate.format(
+        'dddd, MMMM D'
+    );
+
     return dateString;
+}
+
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
 }
